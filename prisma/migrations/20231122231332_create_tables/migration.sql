9c6b21bff90e322b8ab81c274776a71c19dbccae
tiblazy <hauntedapply@gmail.com>
2023-11-22 20:14:50 -0300

    feat: create tables user, phones

---
+++ prisma/migrations/20231122231332_create_tables/migration.sql
@@ -0,0 +1,29 @@
+-- CreateTable
+CREATE TABLE "usuarios" (
+    "id" TEXT NOT NULL,
+    "nome" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "senha" TEXT NOT NULL,
+    "data_criacao" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "data_atualizacao" TIMESTAMP(3),
+    "ultimo_login" TIMESTAMP(3)
+);
+
+-- CreateTable
+CREATE TABLE "telefones" (
+    "id" SERIAL NOT NULL,
+    "numero" VARCHAR(9) NOT NULL,
+    "ddd" VARCHAR(2) NOT NULL,
+    "userId" TEXT,
+
+    CONSTRAINT "telefones_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "usuarios_id_key" ON "usuarios"("id");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "telefones_id_key" ON "telefones"("id");
+
+-- AddForeignKey
+ALTER TABLE "telefones" ADD CONSTRAINT "telefones_userId_fkey" FOREIGN KEY ("userId") REFERENCES "usuarios"("id") ON DELETE SET NULL ON UPDATE CASCADE;
